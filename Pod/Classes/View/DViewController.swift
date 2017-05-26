f16e677b3479f9208dfccb4872556eb9d06c90de
ApterKing <wangcccong@outlook.com>
2017-05-26 13:51:39 +0800

    修改View

--- Pod/Classes/View/DViewController.swift
+++ Pod/Classes/View/DViewController.swift
@@ -8,24 +8,23 @@
 
 import UIKit
 
-class DViewController: UIViewController {
+public class DViewController: UIViewController {
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.title = "组件D"
-        
+
         let label = UILabel(frame: CGRect(x: (UIScreen.main.bounds.size.width - 120) / 2.0, y: 200, width: 120, height: 50))
         label.text = "这里是组件D测试"
         self.view.addSubview(label)
     }
 
-    override func didReceiveMemoryWarning() {
+    public override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
--- Verify+D.podspec
+++ Verify+D.podspec
@@ -3,7 +3,7 @@ Pod::Spec.new do |s|
 
   # ―――  Spec Metadata  ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
   s.name         = "Verify+D"
-  s.version      = "0.0.1"
+  s.version      = "0.0.2"
   s.summary      = "组件D"
 
   s.homepage     = "https://github.com/ApterKingRepo/Verify-D"
